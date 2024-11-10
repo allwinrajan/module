2a597ae0031ee16b23d067447aa7ada13229dac8
alvinirudayarajan <163723809+allwinrajan@users.noreply.github.com>
2024-11-10 11:13:36 -0800

    v2

--- src/main/java/signup/App.java
+++ src/main/java/signup/App.java
@@ -3,5 +3,6 @@ package signup;
 public class App {
     public static void main(String[] args) {
         System.out.println("I am Alvin Irudaya Rajan");
+        System.out.println("I Create New Maven Project And Uploaded to GitHub Repo From Vs Code");
     }
 }
